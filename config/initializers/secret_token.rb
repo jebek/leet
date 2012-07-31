1223c10c112ff87b576d0df1c9a610e398fa69ed
DJ <jebekkim@gmail.com>
2012-07-30 20:13:07 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Leet::Application.config.secret_token = '4ce15c450d46dec0a974740450e448fb7c2ef861dfb875ffc544ebdf02f5c9e0234fe259b52d0e17cbc14fa54fa4267b3ba986456a5502fef6afc5a7dea7965b'
