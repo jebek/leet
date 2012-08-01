ba98045a103b429cb3cd631410c7810472662b25
DJ <jebekkim@gmail.com>
2012-07-31 18:31:13 -0700

    m

--- app/controllers/vsets_controller.rb
+++ app/controllers/vsets_controller.rb
@@ -1,46 +1,46 @@
 class VsetsController < ApplicationController
   def index
-    @Vsets = Vset.all
+    @vsets = Vset.all
   end
   
   def new
   end
   
   def create
-    @Vset = Vset.create!(params[:Vset])
-    flash[:notice] = "#{@Vset.title} was succesfully created."
+    @vset = Vset.create!(params[:Vset])
+    flash[:notice] = "#{@vset.title} was succesfully created."
     redirect_to Vsets_path
   end
   
   def edit
-    @Vset = Vset.find(params[:id])
+    @vset = Vset.find(params[:id])
   end
   
   def update
-    @Vset = Vset.find(params[:id])
-    @Vset.update_attributes!(params[:Vset])
-    flash[:notice] = "#{@Vset.title} was successfully updated."
-    redirect_to Vset_path(@Vset)
+    @vset = Vset.find(params[:id])
+    @vset.update_attributes!(params[:Vset])
+    flash[:notice] = "#{@vset.title} was successfully updated."
+    redirect_to vset_path(@vset)
   end
   
   def destroy
-    @Vset = Vset.find(params[:id])
-    @Vset.destroy
-    flash[:notice] = "#{@Vset.title} was successfully deleted."
-    redirect_to Vsets_path
+    @vset = Vset.find(params[:id])
+    @vset.destroy
+    flash[:notice] = "#{@vset.title} was successfully deleted."
+    redirect_to vsets_path
   end
   
   def show
-    @Vset = Vset.find(params[:id])
+    @vset = Vset.find(params[:id])
   end
   
   def search_quizlet
-    @Vsets = Vset.find_in_quizlet(params[:search_terms])
+    @vsets = Vset.find_in_quizlet(params[:search_terms])
  
   end
   
   def quiz
-    @Vsets = Vset.all.shuffle
+    @vsets = Vset.all.shuffle
     #@questions = @Vsets.map { |a| a.title }
     #@answers = @Vsets.map { |a| a.rating }
     #@answer_choices = @answers.map { |a| [a, "b", "c", "d"].shuffle }
