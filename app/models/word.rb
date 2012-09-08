7d1b25418557de9903e2084ea1e08855850e1f3d
DJ <jebekkim@gmail.com>
2012-09-07 23:21:29 -0700

    m

--- app/models/word.rb
+++ app/models/word.rb
@@ -1,7 +1,7 @@
 class Word < ActiveRecord::Base
   attr_accessible :name, :definition, :vset_id
   belongs_to :vset
-  has_many :results, :dependent => "destroy"
+  has_many :results, :dependent => :destroy
   has_many :users, :through => :results
   
  
